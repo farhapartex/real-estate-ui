3e032be6a8f480d431784484b34f431913313637
Md Nazmul Hasan <farhapartex@gmail.com>
2025-03-18 03:18:54 +0600

    (feat) added AuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,62 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { authService } from '../api/authService';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const checkAuthStatus = async () => {
+            try {
+                const token = localStorage.getItem("token")
+                if (!token) {
+                    setLoading(false);
+                    return
+                }
+
+                const response = await authService.me();
+                if (response.success) {
+                    setUser(response.user)
+                    setIsAuthenticated(true)
+                } else {
+                    localStorage.removeItem("token")
+                    localStorage.removeItem("refrechToken")
+                }
+            } catch (error) {
+                console.error("Auth check error: ", error)
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuthStatus();
+    }, []);
+
+
+    useEffect(() => {
+        setIsAuthenticated(!!user);
+    }, [user]);
+
+
+    return (
+        <AuthContext.Provider value={{ user, setUser, isAuthenticated, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+
+export const useAuth = () => {
+    const context = React.useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
